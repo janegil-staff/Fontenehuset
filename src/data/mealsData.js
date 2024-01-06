19e45a90a78963b1b80061b85c1ef856cab0f9be
Jan Egil Staff Øvretveit <janstove@gmail.com>
2024-01-06 17:48:39 +0100

    Added weakly lunch menu

---
+++ src/data/mealsData.js
@@ -0,0 +1,29 @@
+const mealsData = [
+  {
+    id: 1,
+    day: "Mandag",
+    meal: "Lasagne",
+  },
+  {
+    id: 2,
+    day: "Tirsdag",
+    meal: "Laks",
+  },
+  {
+    id: 3,
+    day: "Onsdag",
+    meal: "Tikkamasala",
+  },
+  {
+    id: 4,
+    day: "Torsdag",
+    meal: "Torsk",
+  },
+  {
+    id: 5,
+    day: "Fredag",
+    meal: "Pizza",
+  },
+];
+
+export default mealsData;
--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -1,12 +1,72 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import mealsData from "../data/mealsData";
 
-const MenuScreen = () => {
+const Item = (item) => {
   return (
     <View>
-      <Text>Menu Screen</Text>
+      <View style={styles.item}>
+        <Text style={styles.day}>{item.day}</Text>
+      </View>
+      <View style={styles.item}>
+        <Text style={styles.meal}>{item.meal}</Text>
+      </View>
     </View>
   );
 };
 
+const MenuScreen = () => {
+  return (
+    <View>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../../assets/images/Rectangle-23.png")}
+        />
+      </View>
+      <View>
+        <Text style={styles.menuHeader}>Ukemeny</Text>
+      </View>
+      <View style={styles.menuCard}>
+        <FlatList
+          data={mealsData}
+          renderItem={({ item }) => <Item day={item.day} meal={item.meal} />}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  imageContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+  image: {
+    width: "100%",
+  },
+  menuHeader: {
+    textAlign: "center",
+    fontSize: 32,
+    marginVertical: 40,
+    fontWeight: "bold",
+  },
+  menuCard: {
+    borderColor: "black",
+    borderWidth: 1,
+    margin: 20,
+    paddingHorizontal: 50,
+    paddingVertical: 30,
+    borderRadius: 25,
+  },
+  day: {
+    textDecorationLine: "underline",
+    lineHeight: 30,
+    fontWeight: "bold",
+  },
+  meal: {
+    lineHeight: 30,
+  },
+});
 export default MenuScreen;
